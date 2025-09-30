c2db7cfa970fb551fb4cc12164b29afb1f8142ef
healthbiodatascientist <susan.rarity-middleton@outlook.com>
2025-09-30 14:46:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 import dash
 from dash import Dash, html, dcc
 
-app = Dash(__name__, use_pages=True)
+app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True)
 
 app.layout = html.Div([
     html.H1('Multi-page app with Dash Pages'),
