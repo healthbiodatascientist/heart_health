d6a3b7777d4f4d587c38025fa92e4d4513755268
healthbiodatascientist <susan.rarity-middleton@outlook.com>
2025-09-30 16:51:20 +0100

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,8 +1,21 @@
 import dash
-from dash import html
+from dash import dcc, html
+import dash_player as dp
 
 dash.register_page(__name__, path='/')
 
 layout = html.Div([
     html.H1('Home page'),
+    html.Summary("View the interactive data map and timeseries heart disease related data by Scottish NHS board region"),
+    html.Div(
+                    style={"width": "48%", "padding": "0px"},
+                    children=[
+                        dp.DashPlayer(
+                            id="player",
+                            url="https://github.com/healthbiodatascientist/heart_health/raw/refs/heads/main/heart_prev_mapped_video.mp4",
+                            controls=True,
+                            width="100%",
+                            height="250px",
+                        )]
+   )
 ])
